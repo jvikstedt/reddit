ee19f1290b7bc208a59036c8f5ea46f68354ff50
Janne Vikstedt <janne.vikstedt@gmail.com>
2016-02-18 21:56:31 +0200

    Refactoring and renaming redmitapi to just reddit

---
+++ reddit.go
@@ -0,0 +1,20 @@
+package reddit
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func FetchSubReddit(subReddit string) (SubReddit, int) {
+	var data SubReddit
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://www.reddit.com/r/"+subReddit+".json", nil)
+	req.Header.Set("User-Agent", "Redmit")
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err == nil {
+		json.NewDecoder(resp.Body).Decode(&data)
+	}
+	return data, resp.StatusCode
+}
--- subreddit.go
+++ subreddit.go
@@ -1,9 +1,4 @@
-package redmitapi
-
-import (
-	"encoding/json"
-	"net/http"
-)
+package reddit
 
 type SubReddit struct {
 	Data struct {
@@ -69,18 +64,4 @@ type SubReddit struct {
 		Modhash string `json:"modhash"`
 	} `json:"data"`
 	Kind string `json:"kind"`
-}
-
-func FetchSubreddit(subReddit string) (SubReddit, int) {
-	var data SubReddit
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://www.reddit.com/r/"+subReddit+".json", nil)
-	req.Header.Set("User-Agent", "Redmit")
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-	if err == nil {
-		json.NewDecoder(resp.Body).Decode(&data)
-	}
-	return data, resp.StatusCode
 }
